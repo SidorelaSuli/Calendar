cc72f5a0f27e27a4dee9cf365da11933da269c11
SidorelaSuli <sidorela.suli@gmail.com>
2018-11-27 21:01:52 +0100

    Update calender.c

--- calender.c
+++ calender.c
@@ -18,23 +18,20 @@ struct Remainder R;
 
 COORD xy = {0, 0};
 
-void gotoxy (int x, int y)
-{
+void gotoxy (int x, int y){
         xy.X = x; xy.Y = y; // X and Y coordinates
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), xy);
 }
 
 //This will set the forground color for printing in a console window.
-void SetColor(int ForgC)
-{
+void SetColor(int ForgC){
      WORD wColor;
      //We will need this handle to get the current background attribute
      HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
      CONSOLE_SCREEN_BUFFER_INFO csbi;
 
      //We use csbi for the wAttributes word.
-     if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
-     {
+     if(GetConsoleScreenBufferInfo(hStdOut, &csbi)){
         //Mask out all but the background attribute, and add in the forgournd color
           wColor = (csbi.wAttributes & 0xF0) + (ForgC & 0x0F);
           SetConsoleTextAttribute(hStdOut, wColor);
@@ -46,8 +43,7 @@ void ClearColor(){
     SetColor(15);
 }
 
-void ClearConsoleToColors(int ForgC, int BackC)
-{
+void ClearConsoleToColors(int ForgC, int BackC){
      WORD wColor = ((BackC & 0x0F) << 4) + (ForgC & 0x0F);
      //Get the handle to the current output buffer...
      HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -63,8 +59,7 @@ void ClearConsoleToColors(int ForgC, int BackC)
      CONSOLE_SCREEN_BUFFER_INFO csbi;
      //Here we will set the current color
      SetConsoleTextAttribute(hStdOut, wColor);
-     if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
-     {
+     if(GetConsoleScreenBufferInfo(hStdOut, &csbi)){
           //This fills the buffer with a given character (in this case 32=space).
           FillConsoleOutputCharacter(hStdOut, (TCHAR) 32, csbi.dwSize.X * csbi.dwSize.Y, coord, &count);
 
@@ -75,8 +70,7 @@ void ClearConsoleToColors(int ForgC, int BackC)
      return;
 }
 
-void SetColorAndBackground(int ForgC, int BackC)
-{
+void SetColorAndBackground(int ForgC, int BackC){
      WORD wColor = ((BackC & 0x0F) << 4) + (ForgC & 0x0F);;
      SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), wColor);
      return;
